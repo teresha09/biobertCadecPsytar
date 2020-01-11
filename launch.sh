d67b7d82637f1fb33c53030adff10e44f67a0a05
teresha09 <egorfineev@gmail.com>
2020-01-11 16:00:34 +0300

    fix path

--- launch.sh
+++ launch.sh
@@ -42,8 +42,8 @@ python3 run_ner.py --do_train=true --do_eval=true --vocab_file=${SCRIPTPATH}/BIO
     --num_train_epochs=50.0 \
     --data_dir="${cadecarr[$i]}" \
     --output_dir=$outputdir
-cp ${TMP_DIR}/cadec_fold_0${i}_cadec_test -R /${TMP_DIR}/cadec_fold_0${i}_psytar_test
-outputdir1=/${TMP_DIR}/cadec_fold_0${i}_psytar_test
+cp ${TMP_DIR}/cadec_fold_0${i}_cadec_test -R ${TMP_DIR}/cadec_fold_0${i}_psytar_test
+outputdir1=${TMP_DIR}/cadec_fold_0${i}_psytar_test
 python3 run_ner.py --do_train=false --do_predict=true --do_eval=true --vocab_file=${SCRIPTPATH}/BIOBERT_DIR/vocab.txt \
     --bert_config_file=${SCRIPTPATH}/BIOBERT_DIR/bert_config.json \
     --init_checkpoint=${SCRIPTPATH}/BIOBERT_DIR/biobert_model.ckpt \
